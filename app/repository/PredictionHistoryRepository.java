c911a359565df290fb6fde5df95ff237fff813eb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-28 04:35:36 +0100

    Update PredictionHistoryRepository.java

--- app/repository/PredictionHistoryRepository.java
+++ app/repository/PredictionHistoryRepository.java
@@ -2,6 +2,8 @@ package com.example.blockchainvoting.repository;
 
 import com.example.blockchainvoting.model.PredictionHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface PredictionHistoryRepository extends JpaRepository<PredictionHistory, Long> {
+    List<PredictionHistory> findByDayOfWeek(int dayOfWeek);
 }

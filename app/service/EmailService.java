c629d4b704d3c424b7825d5c911a475545514e4d
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-28 05:24:44 +0100

    Update EmailService.java

--- app/service/EmailService.java
+++ app/service/EmailService.java
@@ -3,7 +3,11 @@ package com.example.blockchainvoting.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 
 @Service
 public class EmailService {
@@ -17,5 +21,19 @@ public class EmailService {
         message.setSubject(subject);
         message.setText(text);
         mailSender.send(message);
+    }
+
+
+    public void sendHtmlEmail(String to, String subject, String htmlContent) {
+        MimeMessage mimeMessage = mailSender.createMimeMessage();
+        try {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(htmlContent, true); 
+            mailSender.send(mimeMessage);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
     }
 }

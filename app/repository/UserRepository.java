781d7f933489880eb0ba653c66bb5e0409794f32
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2024-11-28 05:33:51 +0100

    Update UserRepository.java

--- app/repository/UserRepository.java
+++ app/repository/UserRepository.java
@@ -6,4 +6,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsername(String username);
     User findByConfirmationToken(String confirmationToken); 
-}
